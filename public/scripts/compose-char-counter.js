baefb5163efc47c9036b344a7874f3e4aa03aff9
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-02-02 16:18:18 -0700

    updated-counter

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,19 +1,12 @@
-$(document).ready(function() {
-
-
-$('#tweet-text').on("input", function() {
-  let count = $(this).val().length;
-
-if (Number($('.counter').html()) < 0) {
-  $('.counter').css("color", "red");
-  $('.counter').html(140 - count);
-} else { 
-  $('.counter').css("color", "inherit");
-$('.counter').html(140 - count);
-
-}
-
-});
-
-
+$(document).ready(function () {
+  $("#tweet-text").on("input", function () {
+    let count = $(this).val().length;
+    if (Number(count) > 140) {
+      $(".counter").css("color", "red");
+      $(".counter").html(140 - count);
+    } else if (Number(count) <= 140) {
+      $(".counter").css("color", "inherit");
+      $(".counter").html(140 - count);
+    }
+  });
 });
